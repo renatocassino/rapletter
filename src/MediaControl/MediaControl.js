af290a8fcb5cb91b50eaad7e25e01124f34140f1
Tacnoman Cassino <renatocassino@gmail.com>
2018-03-13 18:44:30 -0300

    Button to change volume in song

--- src/MediaControl/MediaControl.js
+++ src/MediaControl/MediaControl.js
@@ -4,6 +4,31 @@ import { Icon } from 'react-fa'
 import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar'
 import RaisedButton from 'material-ui/RaisedButton'
 import Slider from 'material-ui/Slider'
+import { withState } from 'recompose'
+
+const volumes = [
+  {value: 0, icon: 'off'},
+  {value: 0.5, icon: 'down'},
+  {value: 1, icon: 'up'}
+]
+
+const VolumeButton = withState('volume', 'setVolume', 2)(({
+  volume,
+  setVolume
+}) => {
+  const currentVolume = volumes[volume]
+
+  return (
+    <RaisedButton onClick={() => {
+      const newIdxVolume = volume+1 >= volumes.length ? 0 : volume+1
+      const newVolume = volumes[newIdxVolume]
+      window.wavesurfer.setVolume(newVolume.value)
+      setVolume(newIdxVolume)
+    }}>
+      <Icon name={`volume-${currentVolume.icon}`} />
+    </RaisedButton>
+  )
+})
 
 const MediaControl = ({
   wavesurfer = {}
@@ -16,6 +41,7 @@ const MediaControl = ({
         <ToolbarGroup>
           <RaisedButton onClick={() => wavesurfer.playPause()}><Icon name={isPlaying ? 'pause' : 'play'} /></RaisedButton>
           <RaisedButton onClick={() => wavesurfer.stop()}><Icon name="stop" /></RaisedButton>
+          <VolumeButton />
         </ToolbarGroup>
       </Toolbar>
       <Slider
